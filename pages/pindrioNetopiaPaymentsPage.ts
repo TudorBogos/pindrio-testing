bcb2aabb6e12a7e68b02d223e340f99eb29a012b
Andrei Munteanu <andreimunteanu7@yahoo.com>
2025-08-13 12:43:06 +0300

    fix: checkout modifications

--- pages/pindrioNetopiaPaymentsPage.ts
+++ pages/pindrioNetopiaPaymentsPage.ts
@@ -10,6 +10,8 @@ export class pindrioNetopiaPaymentsPage {
     readonly cardNameField: Locator;
     readonly payButton: Locator;
     readonly returnToStoreButton: Locator;
+    readonly phoneNRField: Locator;
+    readonly continueButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
@@ -19,11 +21,27 @@ export class pindrioNetopiaPaymentsPage {
         this.cardNameField = page.getByRole('textbox', { name: 'Maria Popescu' });
         this.payButton = page.getByRole('button', { name: /^Plătește \d+\.\d+ RON$/ });
         this.returnToStoreButton = page.getByRole('link', { name: 'Înapoi la magazin' });
+        this.phoneNRField=page.getByRole('textbox', { name: '07xxxxxxxx' });
+        this.continueButton = page.getByRole('button', { name: /^Continuă$/ });
     }
 
     async fillCardInfo(ctx: TestContext) {
         await this.page.waitForLoadState('load');
         await this.page.waitForTimeout(5000);
+
+        if (await this.phoneNRField.isVisible()) {
+            await this.phoneNRField.fill(ctx.phone);
+        }
+
+
+        if (await this.continueButton.isVisible()){
+            await expect (this.continueButton).toBeEnabled();
+            await this.continueButton.click();
+        }
+
+
+        await this.page.waitForLoadState('load');
+        await this.page.waitForTimeout(2000);
 
         await expect(this.cardNameField).toBeVisible();
         await this.cardNameField.fill(ctx.cardName);
@@ -39,7 +57,7 @@ export class pindrioNetopiaPaymentsPage {
 
         await expect(this.payButton).toBeEnabled();
         await this.payButton.click();
-        await this.page.waitForTimeout(7000);
+        await this.page.waitForTimeout(15000);
         return new pindrioOrderConfirmedPage(this.page);
     }
 }
